8406cfdbd49e40a4ccc1cac68009ec3494595dd8
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-01-25 16:54:05 +0100

    Update index.js

--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -23,92 +23,94 @@ const router = express.Router();
 require("dotenv/config");
 
 function authenticateToken(req, res, next) {
-	const authHeader = req.headers["authorization"]
-	const token = authHeader && authHeader.split(" ")[1]
-	if (token == null) return res.sendStatus(401)
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		if (err) return res.sendStatus(403)
-		req.user = user
-		next()
-	})
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
-router.get("/", authenticateToken,async (req, res, next) => {
-    try {
-        const profiles = await profileSchema.find();
-        ///const resp = res.json(profiles.filter(profile => profile.username === req.user.name))
-        res.send(profiles)
-    } catch (error) {
-        next(error) 
-    }
-})
+router.get("/", authenticateToken, async (req, res, next) => {
+  try {
+    const profiles = await profileSchema.find();
+    ///const resp = res.json(profiles.filter(profile => profile.username === req.user.name))
+    res.send(profiles);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/me", authenticateToken, async (req, res, next) => {
-	try {
-		const profiles = await profileSchema.find()
-		const resp = res.json(
-			profiles.filter((profile) => profile.username === req.user.name)
-		)
-		res.send(resp)
-	} catch (error) {
-		next(error)
-	}
-})
+  try {
+    const profiles = await profileSchema.find();
+    const resp = res.json(
+      profiles.filter((profile) => profile.username === req.user.name)
+    );
+    res.send(resp);
+  } catch (error) {
+    next(error);
+  }
+});
 /**router.post("/:id/picture", authenticateToken, async (req, res, next) => {
     const postPic = await profileSchema.findById(req.params.id)
     
     })*/
-    router.post("/", async (req, res, next) => {
-        try {
-            const postProfile = new profileSchema(req.body)
-            const { _id } = await postProfile.save()
-            const username = req.body.username
-            const user = { name: username }
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-            res.json({ accessToken: accessToken })
-            res.status(201).send(_id)
-        } catch (error) {
-            next(error)
-        }
-    })
-    router.get("/:id", authenticateToken, async (req, res, next) => {
-        try {
-            const profile = await profileSchema.findById(req.params.id);
-            res.send(profile)
-        } catch (error) {
-            next(error)
-        }
-    })
-    router.put("/:id", authenticateToken, async (req, res, next ) => {
-        try {
-            const profile = await profileSchema.findByIdAndUpdate(
-                req.params.id,req.body,{ runValidators: true, new: true}
-            )
-            if (profile) {
-                res.send(profile)
-            } else {
-                const err = new Error("Profile not found");
-                err.httpStatusCode = 404;
-                next(error)
-            }
-        } catch (error) {
-            next(error)
-        }
-    })
-router.delete("/:id", authenticateToken,async(req, res, next) => {
-    try {
-        const profile = await profileSchema.findByIdAndDelete(req.params.id);
-        if (profile) {
-            res.send("deleted")
-        } else {
-            const error = new Error(`Profile with ${req.params.id} is not found`)
-            error.httpStatusCode = 404
-            next(error) 
-        }
-    } catch (error) {
-        next(error)
-    }}
+router.post("/", async (req, res, next) => {
+  try {
+    const postProfile = new profileSchema(req.body);
+    const { _id } = await postProfile.save();
+    const username = req.body.username;
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+    res.status(201).send(_id);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:id", authenticateToken, async (req, res, next) => {
+  try {
+    const profile = await profileSchema.findById(req.params.id);
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+});
+router.put("/:id", authenticateToken, async (req, res, next) => {
+  try {
+    const profile = await profileSchema.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { runValidators: true, new: true }
     );
+    if (profile) {
+      res.send(profile);
+    } else {
+      const err = new Error("Profile not found");
+      err.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete("/:id", authenticateToken, async (req, res, next) => {
+  try {
+    const profile = await profileSchema.findByIdAndDelete(req.params.id);
+    if (profile) {
+      res.send("deleted");
+    } else {
+      const error = new Error(`Profile with ${req.params.id} is not found`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
