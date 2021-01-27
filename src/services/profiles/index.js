ed262db252ec891ce14dd11bd4fd9e974d49e2b2
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-01-27 09:38:43 +0100

    cloudinary

--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -17,9 +17,9 @@ Generates and download a PDF with the CV of the user (details, picture, experien
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const profileSchema = require("./mongo");
-const cloudinary = require("../../utils/cloudinary");
 const multer = require("multer")
 const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const {cloudinary} = require("../../utils/cloudinary");
 const cloudStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
