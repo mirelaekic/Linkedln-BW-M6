a67df5a94e6fda5c4c4e2c6073a60dfae79a4b46
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-01-28 16:54:49 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ server.use(genericErrorHandler)
 console.log(listEndpoints(server))
 
 mongoose
-	.connect(process.env.MONGO_CONNECTION, {
+	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
